9f17ef47d49e21a9ae4395c4a53b01ba022e2505
Luiz Moura <muniz.mourafelipe@gmail.com>
2024-04-03 10:01:24 -0300

    Add netlify.toml file for CORS configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
-const { LoaderOptionsPlugin } = require("webpack");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -12,17 +11,6 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
-    externals: ["@Chef/carousel", "@Chef/styleguide", "@Chef/utility", "single-spa", "rxjs"],
-    plugins: [
-      new LoaderOptionsPlugin({
-        options: {
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
-            "Access-Control-Allow-Headers": "*"
-          }
-        }
-      })
-    ]
+    externals: ["@Chef/carousel", "@Chef/styleguide", "@Chef/utility", "single-spa", "rxjs"]
   });
 };
